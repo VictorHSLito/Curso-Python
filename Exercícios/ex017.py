28b28166699335effcdf96a3b6ee9213efda842a
HugoLito <victorferrarihugo@hotmail.com>
2023-09-20 19:31:54 -0300

    Resolução do Ex 102

--- Exercícios/ex017.py
+++ Exercícios/ex017.py
@@ -1,7 +1,7 @@
 # Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa
 # Retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 
-import time
+'''import time
 import datetime
 
 def voto(ano):
@@ -23,4 +23,29 @@ print("Iremos ver agora se você tem idade suficiente para votar ou não")
 print("Aguarde um instante")
 time.sleep(2)
 voto(ano)
-print(f"Após as análises {nome}, concluímos que você tem {idade} anos e o voto esse ano é {voto(ano)}")
+print(f"Após as análises {nome}, concluímos que você tem {idade} anos e o voto esse ano é {voto(ano)}")'''
+
+# Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular
+# E outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+
+
+def fatorial(num, mostrar=''):
+    """
+    Calcula o Fatorial de um número
+    :param num: Para o fatorial do número ao ser calculado
+    :param mostrar: Para mostrar a multiplicação ou não
+    :return: Retorna o resultado
+    """
+    fat = 1
+    for i in range(num, 0, -1):
+        if mostrar:
+            print(f"{i}", end='')
+            if i > 1:
+                print(" x ", end='')
+            else:
+                print(" = ", end='')
+        fat = fat*i
+    return fat
+
+
+print(fatorial(5, True))
