86d3301e56eb8031af1bdef2c47aa955e82e7243
HugoLito <victorferrarihugo@hotmail.com>
2023-09-24 15:21:27 -0300

    Aula 22

---
+++ Aulas/Aula 22.py
@@ -0,0 +1,11 @@
+# Módulos e Pacotes
+
+# Crie um arquivo com determinadas funcionalidades e importe elas no código principal
+
+import uteis
+
+num = int(input("Digite um número: "))
+fat = uteis.fatorial(num)
+print(f"O fatorial do número {num} é {fat}")
+print(f"O dobro do número {num} é {uteis.dobro(num)}")
+print(f"O triplo do número {num} é {uteis.dobro(num)}")
