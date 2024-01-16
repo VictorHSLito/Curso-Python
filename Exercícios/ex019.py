e070b71d65e00ecc450366edecc4145da2616df4
Victor Hugo <victorferrarihugo@hotmail.com>
2024-01-15 21:02:25 -0300

    Resolução do Exercício 110

--- Exercícios/ex019.py
+++ Exercícios/ex019.py
@@ -1,6 +1,6 @@
-# Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais,
-# informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108
-import moeda
+# Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais
+# Informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108
+'''import moeda
 
 preço = float(input("Digite um preço: R$"))
 
@@ -8,4 +8,12 @@ print(f'A metade de {moeda.moeda(preço)} é {moeda.metade(preço, True)}')
 print(f'O dobro de {moeda.moeda(preço)} é {moeda.dobro(preço, True)}')
 print(f'O triplo de {moeda.moeda(preço)} é {moeda.triplo(preço, True)}')
 print(f'O aumento de 10% de {moeda.moeda(preço)} é {moeda.aumento(preço, True)}')
-print(f'Reduzindo 12% de {moeda.moeda(preço)} é {moeda.diminuir(preço, 12, True)}')
+print(f'Reduzindo 12% de {moeda.moeda(preço)} é {moeda.diminuir(preço, 12, True)}')'''
+
+# Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo()
+# Que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui
+
+import moeda
+
+preço = float(input("Digite um preço: R$ "))
+moeda.resumo(preço)
--- Exercícios/moeda.py
+++ Exercícios/moeda.py
@@ -24,3 +24,14 @@ def moeda(preço = 0, moeda = 'R$'):
 def diminuir(preço, arg, formato= False):
     res = preço * (1 - arg/100)
     return res if not formato else moeda(res)
+
+def resumo(preço=0, taxa_a=10, taxa_r=5):
+    print('-' * 30)
+    print("RESUMO DO VALOR".center(30))
+    print('-'*30)
+    print(f'Preço analisado: {moeda(preço)}')
+    print(f'Dobro do preço: {dobro(preço, True)}')
+    print(f'Metade do preço: R${metade(preço, True)}')
+    print(f'Com {taxa_a}% de aumento: {aumento(preço, taxa_a,True)}')
+    print(f'Com {taxa_r}% de redução: {diminuir(preço, taxa_r, True)}')
+    print('-'*30)
