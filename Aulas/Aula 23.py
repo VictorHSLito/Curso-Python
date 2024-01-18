526f56805a13ce40720d82db788ec2aa7dfc9109
Victor Hugo <victorferrarihugo@hotmail.com>
2024-01-17 22:09:36 -0300

    Aula 23

---
+++ Aulas/Aula 23.py
@@ -0,0 +1,13 @@
+# Estrutura Try e Except:
+
+try:
+    a = int(input("Numerador: "))
+    b = int(input("Denominador: "))
+    r = a/b
+except Exception as erro:
+    print(f"Problema encontrado foi {erro.__class__}")
+else:
+    print(f"O resultado é {r:.1f}")
+finally:
+    print("Volte sempre!")
+    
