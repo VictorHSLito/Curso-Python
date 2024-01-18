6a3210119557217cf52a69454c53788f746051ee
Victor Hugo <victorferrarihugo@hotmail.com>
2024-01-17 22:13:57 -0300

    Aula 23

--- Aulas/Aula 23.py
+++ Aulas/Aula 23.py
@@ -4,10 +4,15 @@ try:
     a = int(input("Numerador: "))
     b = int(input("Denominador: "))
     r = a/b
+except (ValueError, TypeError):
+    print("Tivemos um problema com os tipos de dados que você digitou")
+except ZeroDivisionError:
+    print("Você tentou dividr por zero!")
+except KeyboardInterrupt:
+    print("O usuário não digitou nada!")
 except Exception as erro:
-    print(f"Problema encontrado foi {erro.__class__}")
+    print(f"O erro foi de {erro.__cause__}")
 else:
     print(f"O resultado é {r:.1f}")
 finally:
     print("Volte sempre!")
-    
